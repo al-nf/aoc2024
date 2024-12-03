d4f30bdf199a0e204e5bfc4f9e4ed30d3f50c7fd
afung <afung@babylon.local>
2024-12-02 18:11:39 -0800

    day2, part1 done

--- day2/part1.c
+++ day2/part1.c
@@ -1,31 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 1024
 
 int main(int argc, char *argv[])
 {
     FILE *fp;
-    int i, inp, k, sum, diff1, diff2;
-
+    char buffer[MAX_LINE_LENGTH];
+    int inp, linp, diff;
+    int sum = 0;
+    bool isFirst, good, isIncreasing, isDecreasing;
 
     if (argc != 2)
     {
-        fprintf(stderr, "????\n");
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
     if ((fp = fopen(argv[1], "r")) == NULL)
     {
-        fprintf(stderr, "cannot open %s\n", argv[1]);
+        fprintf(stderr, "Cannot open file: %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    assert(fp != NULL);
 
-    while (fscanf(fp, "%d", &inp) != EOF)
+    // whoever made fgets needs to be viciously punished
+    while (fgets(buffer, MAX_LINE_LENGTH, fp) != NULL)
     {
-        if (inp == EOL)
+        char *ptr = buffer; 
+        good = true;
+        isFirst = true;
+        isIncreasing = false;
+        isDecreasing = false;
+
+        while (sscanf(ptr, "%d", &inp) == 1)
         {
-            
+            if (isFirst)
+            {
+                linp = inp; 
+                isFirst = false;
+            }
+            else
+            {
+                diff = inp - linp;
+
+                printf("inp: %d, linp: %d, diff: %d\n", inp, linp, diff);
+
+                if (diff > 3 || diff < -3)
+                {
+                    good = false;
+                    break;
+                }
+
+                if (diff > 0)
+                {
+                    if (isDecreasing)
+                    {
+                        good = false;
+                        break;
+                    }
+                    isIncreasing = true;
+                }
+                else if (diff < 0) 
+                {
+                    if (isIncreasing)
+                    {
+                        good = false;
+                        break;
+                    }
+                    isDecreasing = true;
+                }
+                else
+                {
+                    good = false;
+                    break;
+                }
+
+                linp = inp;
+            }
+
+            while (*ptr != '\0' && *ptr != ' ' && *ptr != '\n') // Skip current number
+                ptr++;
+            while (*ptr == ' ')
+                ptr++;
         }
+
+        if (good && !isFirst) 
+        {
+            sum++;
+        }
+
     }
-    printf("sum: %lld\n", sum);
+
+    fclose(fp);
+    printf("sum: %d\n", sum);
+
+    return 0;
 }
+
