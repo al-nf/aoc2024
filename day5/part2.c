3bbb29d6bf57852322ce97acbdee610df6a6e666
al-nf <alankjfung@gmail.com>
2024-12-04 22:09:05 -0800

    finished part2

--- day5/part2.c
+++ day5/part2.c
@@ -5,13 +5,13 @@
 
 #define MAX_PAGES 100
 
-typedef struct node 
+typedef struct node
 {
     int data;
     struct node *next;
 } NODE;
 
-typedef struct list 
+typedef struct list
 {
     NODE *head;
 } LIST;
@@ -36,7 +36,7 @@ static void addNode(LIST *lp, int data)
 static void destroyList(LIST *lp)
 {
     NODE *np = lp->head->next;
-    while (np != lp->head) 
+    while (np != lp->head)
     {
         NODE *temp = np;
         np = np->next;
@@ -49,26 +49,26 @@ static void destroyList(LIST *lp)
 static int isValid(int *update, int size, LIST **data)
 {
     int pos[MAX_PAGES];
-    for (int i = 0; i < MAX_PAGES; i++) 
+    for (int i = 0; i < MAX_PAGES; i++)
     {
         pos[i] = -1;
     }
 
-    for (int i = 0; i < size; i++) 
+    for (int i = 0; i < size; i++)
     {
         pos[update[i]] = i;
     }
 
-    for (int i = 0; i < MAX_PAGES; i++) 
+    for (int i = 0; i < MAX_PAGES; i++)
     {
         NODE *np = data[i]->head->next;
-        while (np != data[i]->head) 
+        while (np != data[i]->head)
         {
             int page1 = i;
             int page2 = np->data;
-            if (pos[page1] != -1 && pos[page2] != -1 && pos[page1] > pos[page2]) 
+            if (pos[page1] != -1 && pos[page2] != -1 && pos[page1] > pos[page2])
             {
-                return 0; 
+                return 0;
             }
             np = np->next;
         }
@@ -79,45 +79,78 @@ static int isValid(int *update, int size, LIST **data)
 
 static int findMiddlePage(int *update, int size)
 {
-    if (size == 0) 
+    if (size == 0)
     {
         return -1;
     }
     return update[size / 2];
 }
 
-int main(int argc, char *argv[]) 
+static int compare(int page1, int page2, LIST **data)
+{
+    NODE *np = data[page1]->head->next;
+    while (np != data[page1]->head)
+    {
+        if (np->data == page2)
+        {
+            return 1;
+        }
+        np = np->next;
+    }
+    return 0;
+}
+
+static void topoSort(int *update, int size, LIST **data)
+{
+    int swapped;
+    do
+    {
+        swapped = 0;
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (compare(update[i], update[i + 1], data))
+            {
+                int temp = update[i];
+                update[i] = update[i + 1];
+                update[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+    } while (swapped);
+}
+
+int main(int argc, char *argv[])
 {
     FILE *fp1, *fp2;
     int sum = 0;
     LIST **data;
 
-    if (argc != 3) 
+    if (argc != 3)
     {
         exit(EXIT_FAILURE);
     }
 
-    if ((fp1 = fopen(argv[1], "r")) == NULL) 
+    if ((fp1 = fopen(argv[1], "r")) == NULL)
     {
         exit(EXIT_FAILURE);
     }
 
-    if ((fp2 = fopen(argv[2], "r")) == NULL) 
+    if ((fp2 = fopen(argv[2], "r")) == NULL)
     {
         fclose(fp1);
         exit(EXIT_FAILURE);
     }
 
     data = malloc(sizeof(LIST*) * MAX_PAGES); assert(data != NULL);
-    for (int i = 0; i < MAX_PAGES; i++) 
+    for (int i = 0; i < MAX_PAGES; i++)
     {
         data[i] = createList();
     }
 
     int j, k;
-    while (fscanf(fp1, "%d|%d", &j, &k) != EOF) 
+    while (fscanf(fp1, "%d|%d", &j, &k) != EOF)
     {
-        if (j < 0 || j >= MAX_PAGES || k < 0 || k >= MAX_PAGES) 
+        if (j < 0 || j >= MAX_PAGES || k < 0 || k >= MAX_PAGES)
         {
             continue;
         }
@@ -125,21 +158,22 @@ int main(int argc, char *argv[])
     }
 
     char line[256];
-    while (fgets(line, sizeof(line), fp2)) 
+    while (fgets(line, sizeof(line), fp2))
     {
         int update[MAX_PAGES];
         int size = 0;
         char *token = strtok(line, ",");
-        while (token != NULL && size < MAX_PAGES) 
+        while (token != NULL && size < MAX_PAGES)
         {
             update[size++] = atoi(token);
             token = strtok(NULL, ",");
         }
 
-        if (size > 0 && isValid(update, size, data)) 
+        if (size > 0 && !isValid(update, size, data))
         {
+            topoSort(update, size, data);
             int midPage = findMiddlePage(update, size);
-            if (midPage != -1) 
+            if (midPage != -1)
             {
                 sum += midPage;
             }
@@ -149,7 +183,7 @@ int main(int argc, char *argv[])
     fclose(fp1);
     fclose(fp2);
 
-    for (int i = 0; i < MAX_PAGES; i++) 
+    for (int i = 0; i < MAX_PAGES; i++)
     {
         destroyList(data[i]);
     }
