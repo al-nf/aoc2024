9cefd6edf7b6290d6a12db8967eb55d1e3e7bb02
al-nf <alankjfung@gmail.com>
2024-12-11 02:11:29 -0800

    ummmm

--- day11/part2.cpp
+++ day11/part2.cpp
@@ -1,121 +1,65 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
 #include <sstream>
-#include <cmath>
-#include <utility>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-unsigned long long stones(vector<unsigned long long>& data, int reps)
+unordered_map<unsigned long long, vector<unsigned long long>> memo;
+
+vector<unsigned long long> transform(unsigned long long num) 
 {
-    unsigned long long zeroes = 0;
-    unsigned long long ones = 0;
-
-    auto removeZerosAndOnes = [&zeroes, &ones](vector<unsigned long long>& data) 
+    if (memo.find(num) != memo.end()) 
     {
-        auto it = data.begin();
-        while (it != data.end()) 
-        {
-            if (*it == 0) 
-            {
-                zeroes++;
-                it = data.erase(it); 
-            } 
-            else if (*it == 1) 
-            {
-                ones++;
-                it = data.erase(it); 
-            } 
-            else 
-            {
-                ++it;
-            }
-        }
-    };
-
-    removeZerosAndOnes(data);
-
-    auto evenDigits = [](unsigned long long num)
-    {
-        int count = 0;
-        while (num != 0)
-        {
-            num /= 10;
-            count++;
-        }
-        return count % 2 == 0;
-    };
-
-    auto split = [](unsigned long long num) -> pair<unsigned long long, unsigned long long>
-    {
-        if (num == 0)
-        {
-            return {0, 0};
-        }
-
-        int digits = static_cast<int>(log10(num)) + 1;
-        unsigned long long div = 1;
-        for (int i = 0; i < digits / 2; i++)
-        {
-            div *= 10;
-        }
-
-        unsigned long long part1 = num / div;
-        unsigned long long part2 = num % div;
-
-        return {part1, part2};
-    };
-
-    auto blink = [&data, &evenDigits, &split, &zeroes, &ones]() 
-    {
-        vector<unsigned long long> tmp;
-        unsigned long long newZeroes = 0;
-        unsigned long long newOnes = ones; 
-        for (size_t i = 0; i < data.size(); i++) 
-        {
-            if (evenDigits(data[i])) 
-            {
-                pair<unsigned long long, unsigned long long> splitNum = split(data[i]);
-                if (splitNum.first == 0)
-                    newZeroes++;
-                else if (splitNum.first == 1)
-                    newOnes++;
-                else
-                    tmp.push_back(splitNum.first);
-                if (splitNum.second == 0)
-                    newZeroes++;
-                else if (splitNum.second == 1)
-                    newOnes++;
-                else
-                    tmp.push_back(splitNum.second); 
-            } 
-            else 
-            {
-                tmp.push_back(data[i] * 2024);
-            }
-        }
-        for (unsigned long long i = 0; i < ones; i++)
-        {
-            tmp.push_back(2024);
-        }
-        data = move(tmp);
-        zeroes = newZeroes;
-        ones = newOnes;
-    };
-
-    for (int i = 0; i < reps; i++)
-    {
-        /*
-        for (unsigned long long num : data)
-        {
-            cout << num << " ";
-        }
-        cout << endl;
-        */
-        blink();
+        return memo[num];
     }
-    return data.size() + zeroes + ones;
+
+    vector<unsigned long long> result;
+
+    if (num == 0) 
+    {
+        result.push_back(1); 
+    }
+    else 
+    {
+        string num_str = to_string(num);
+        if (num_str.size() % 2 == 0) 
+        {
+            size_t mid = num_str.size() / 2;
+            unsigned long long left = stoull(num_str.substr(0, mid));
+            unsigned long long right = stoull(num_str.substr(mid));
+            result.push_back(left);
+            result.push_back(right);
+        } 
+        else 
+        {
+            result.push_back(num * 2024); 
+        }
+    }
+
+    memo[num] = result;
+
+    return result;
+}
+
+unsigned long long countStones(vector<unsigned long long>& stones, int blinks) 
+{
+    if (blinks == 0) 
+    {
+        return stones.size();
+    }
+
+    vector<unsigned long long> next_stones;
+    for (unsigned long long stone : stones) 
+    {
+        vector<unsigned long long> transformed = transform(stone);
+        next_stones.insert(next_stones.end(), transformed.begin(), transformed.end());
+    }
+
+    return countStones(next_stones, blinks - 1);
 }
 
 int main(int argc, char *argv[]) 
@@ -126,7 +70,7 @@ int main(int argc, char *argv[])
     if (!input_file) 
     {
         cerr << "error opening file" << endl;
-        return 1; 
+        return 1;
     }
 
     string line;
@@ -134,13 +78,12 @@ int main(int argc, char *argv[])
     {
         stringstream ss(line);
         unsigned long long value;
-        
-        while (ss >> value)
-        {
-            data.push_back(value); 
+
+        while (ss >> value) {
+            data.push_back(value);
         }
     }
 
-    printf("stones: %llu\n", stones(data, 25));
+    printf("stones: %llu\n", countStones(data, 75));
 }
 
