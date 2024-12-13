8cd2aaefcfa0d3ad9a849ff7a4b47e1e1deb8832
al-nf <alankjfung@gmail.com>
2024-12-13 04:05:57 -0800

    SLEEPY TIME :3

--- day13/part1.cpp
+++ day13/part1.cpp
@@ -11,7 +11,8 @@
 
 using namespace std;
 
-int extendedGCD(int a, int b, int& x, int& y) {
+int extendedGCD(int a, int b, int& x, int& y) 
+{
     if (b == 0) {
         x = 1;
         y = 0;
@@ -25,7 +26,8 @@ int extendedGCD(int a, int b, int& x, int& y) {
 }
 
 
-vector<tuple<int, int, int>> dioph(int ax, int bx, int c) {
+vector<tuple<int, int, int>> dioph(int ax, int bx, int c) 
+{
     vector<tuple<int, int, int>> solutions;
     int x0, y0;
     int g = extendedGCD(ax, bx, x0, y0);
@@ -41,9 +43,9 @@ vector<tuple<int, int, int>> dioph(int ax, int bx, int c) {
     int stepY = ax / g;
 
     int tMinX = ceil((0 - x0) / (double)stepX);
-    int tMaxX = floor((1000000 - x0) / (double)stepX);
+    int tMaxX = floor((100 - x0) / (double)stepX);
 
-    int tMinY = ceil((y0 - 1000000) / (double)stepY);
+    int tMinY = ceil((y0 - 100) / (double)stepY);
     int tMaxY = floor((y0 - 0) / (double)stepY);
 
     int tMin = max(tMinX, tMinY);
@@ -54,7 +56,7 @@ vector<tuple<int, int, int>> dioph(int ax, int bx, int c) {
         int x = x0 + t * stepX;
         int y = y0 - t * stepY;
         solutions.push_back(make_tuple(x, y, c));
-        printf("Solution found: x = %d, y = %d\n", x, y);
+        //printf("Solution found: x = %d, y = %d\n", x, y);
     }
 
     return solutions;
@@ -143,14 +145,14 @@ int main(int argc, char *argv[])
                     cost = 3 * get<0>(solution) + get<1>(solution);
                     if (cost < minCost)
                     {
-                            printf("minCost set to %d * %d + %d * %d = %d, %d * %d + %d * %d = %d\n", buttonA[i].first, get<0>(solution), buttonB[i].first, get<1>(solution), prize[i].first, buttonA[i].second, get<0>(solution), buttonB[i].second, get<1>(solution), prize[i].second);
-                            printf("Actual answers: %d, %d\n", buttonA[i].first * get<0>(solution) + buttonB[i].first * get<1>(solution), buttonA[i].second * get<0>(solution) + buttonB[i].second * get<1>(solution));
+                            //printf("minCost set to %d * %d + %d * %d = %d, %d * %d + %d * %d = %d\n", buttonA[i].first, get<0>(solution), buttonB[i].first, get<1>(solution), prize[i].first, buttonA[i].second, get<0>(solution), buttonB[i].second, get<1>(solution), prize[i].second);
+                            //printf("Actual answers: %d, %d\n", buttonA[i].first * get<0>(solution) + buttonB[i].first * get<1>(solution), buttonA[i].second * get<0>(solution) + buttonB[i].second * get<1>(solution));
                         minCost = cost;
                     }
                 }
                 else
                 {
-                    printf("Solution x = %d, y = %d is invalid\n", get<0>(solution), get<1>(solution));
+                    //printf("Solution x = %d, y = %d is invalid\n", get<0>(solution), get<1>(solution));
                 }
             }
 
@@ -161,21 +163,21 @@ int main(int argc, char *argv[])
                     cost = 3 * get<0>(solution) + get<1>(solution);
                     if (cost < minCost)
                     {
-                        printf("minCost set to %d * %d + %d * %d = %d, %d * %d + %d * %d = %d\n", buttonA[i].first, get<0>(solution), buttonB[i].first, get<1>(solution), prize[i].first, buttonA[i].second, get<0>(solution), buttonB[i].second, get<1>(solution), prize[i].second);
-                        printf("Actual answers: %d, %d\n", buttonA[i].first * get<0>(solution) + buttonB[i].first * get<1>(solution), buttonA[i].second * get<0>(solution) + buttonB[i].second * get<1>(solution));
+                        //printf("minCost set to %d * %d + %d * %d = %d, %d * %d + %d * %d = %d\n", buttonA[i].first, get<0>(solution), buttonB[i].first, get<1>(solution), prize[i].first, buttonA[i].second, get<0>(solution), buttonB[i].second, get<1>(solution), prize[i].second);
+                        //printf("Actual answers: %d, %d\n", buttonA[i].first * get<0>(solution) + buttonB[i].first * get<1>(solution), buttonA[i].second * get<0>(solution) + buttonB[i].second * get<1>(solution));
                         minCost = cost;
                     }
                 }
                 else
                 {
-                    printf("Solution x = %d, y = %d is invalid\n", get<0>(solution), get<1>(solution));
+                    //printf("Solution x = %d, y = %d is invalid\n", get<0>(solution), get<1>(solution));
                 }
             }
         }
         if (minCost != INT_MAX)
         {
             sum += minCost;
-            printf("added cost %d\n", minCost);
+            //printf("added cost %d\n", minCost);
         }
     }
 
---
+++ day13/part2.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <regex>
+#include <cmath>
+#include <climits>
+#include <numeric>
+#include <algorithm>
+
+#define LL long long
+
+using namespace std;
+
+pair<LL, LL> cramer(LL a1, LL b1, LL a2, LL b2, LL c1, LL c2)
+{
+    return make_pair
+    (
+        (c1*b2 - c2*b1)/(a1*b2 - a2*b1),
+        (a1*c2 - a2*c1)/(a1*b2 - a2*b1)
+    );
+}
+
+int main(int argc, char *argv[]) 
+{
+    ifstream input_file(argv[1]);
+    vector<pair<LL, LL>> buttonA;
+    vector<pair<LL, LL>> buttonB;
+    vector<pair<LL, LL>> prize;
+    LL sum = 0;
+
+    if (!input_file) 
+    {
+        cerr << "error opening file" << endl;
+        return 1;
+    }
+
+    string line;
+    LL idx = 0;
+
+    regex buttonPattern(R"(X[=+](\d+), Y[=+](\d+))");
+    regex prizePattern(R"(X=(\d+), Y=(\d+))");
+
+    while (getline(input_file, line)) 
+    {
+        switch (idx % 4) 
+        {
+            case 0: { 
+                smatch match;
+                if (regex_search(line, match, buttonPattern) && match.size() == 3) 
+                {
+                    LL x = stoll(match[1].str());
+                    LL y = stoll(match[2].str());
+                    buttonA.push_back({x, y});
+                }
+                break;
+            }
+            case 1: { 
+                smatch match;
+                if (regex_search(line, match, buttonPattern) && match.size() == 3) 
+                {
+                    LL x = stoll(match[1].str());
+                    LL y = stoll(match[2].str());
+                    buttonB.push_back({x, y});
+                }
+                break;
+            }
+            case 2: {
+                smatch match;
+                if (regex_search(line, match, prizePattern) && match.size() == 3) 
+                {
+                    LL x = stoll(match[1].str())+10000000000000;
+                    //LL x = stoll(match[1].str());
+                    LL y = stoll(match[2].str())+10000000000000;
+                    //LL y = stoll(match[2].str());
+                    prize.push_back({x, y});
+                }
+                break;
+            }
+            case 3: {
+                break;
+            }
+        }
+        idx++;
+    }
+
+
+    for (LL i = 0; i < buttonA.size(); i++)
+    {
+        pair<LL, LL> buttons = cramer(buttonA[i].first, buttonB[i].first, buttonA[i].second, buttonB[i].second, prize[i].first, prize[i].second);
+        if ((buttonA[i].first * buttons.first + buttonB[i].first * buttons.second == prize[i].first) && (buttonA[i].second * buttons.first + buttonB[i].second * buttons.second == prize[i].second))
+        {
+            sum += (3 * buttons.first + buttons.second);
+            //printf("valid: %ll, %ll\n", buttons.first, buttons.second);
+        }
+        else 
+        {
+            //printf("invalid: %ll, %ll\n", buttons.first, buttons.second);
+        }
+    }
+
+    input_file.close();
+
+    printf("sum: %ll\n", sum);
+}
+
