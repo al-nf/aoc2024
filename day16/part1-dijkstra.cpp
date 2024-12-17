fea2a46197f520ecd6ef7b0e1fa56126073085ab
al-nf <alankjfung@gmail.com>
2024-12-16 17:10:26 -0800

    finished part1 (two algorithms, because neither of them were working)

---
+++ day16/part1-dijkstra.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <set>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+pair<int, int> dir(int direction)
+{
+    switch (direction % 4)
+    {
+        case 0: return {0, 1}; 
+        case 1: return {1, 0};
+        case 2: return {0, -1};
+        case 3: return {-1, 0};
+    }
+}
+
+struct State
+{
+    int g, x, y, direction;
+
+    bool operator>(const State& other) const
+    {
+        return g > other.g;
+    }
+};
+
+int dijkstra(const vector<vector<char>>& map, int startX, int startY, int endX, int endY)
+{
+    int rows = map.size();
+    int cols = map[0].size();
+
+    priority_queue<State, vector<State>, greater<State>> openList;
+    vector<vector<vector<int>>> dist(rows, vector<vector<int>>(cols, vector<int>(4, INT_MAX)));
+    set<tuple<int, int, int>> visited;
+
+    openList.push({0, startX, startY, 0});
+    dist[startX][startY][0] = 0;
+
+    while (!openList.empty())
+    {
+        State cur = openList.top();
+        openList.pop();
+
+        int x = cur.x, y = cur.y, direction = cur.direction;
+        int g = cur.g;
+
+        if (x == endX && y == endY)
+        {
+            return g;
+        }
+
+        pair<int, int> moveDir = dir(direction);
+        int nx = x + moveDir.first, ny = y + moveDir.second;
+
+        if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && map[nx][ny] != '#')
+        {
+            if (g + 1 < dist[nx][ny][direction])
+            {
+                dist[nx][ny][direction] = g + 1;
+                openList.push({g + 1, nx, ny, direction});
+            }
+        }
+
+        int newDirection = (direction + 1) % 4;
+        if (g + 1000 < dist[x][y][newDirection])
+        {
+            dist[x][y][newDirection] = g + 1000;
+            openList.push({g + 1000, x, y, newDirection});
+        }
+
+        newDirection = (direction - 1 + 4) % 4;
+        if (g + 1000 < dist[x][y][newDirection])
+        {
+            dist[x][y][newDirection] = g + 1000;
+            openList.push({g + 1000, x, y, newDirection});
+        }
+    }
+
+    return -1;
+}
+
+void reindeer(vector<vector<char>>& map)
+{
+    int startX, startY, endX, endY;
+    for (int i = 0; i < map.size(); i++)
+    {
+        for (int j = 0; j < map[0].size(); j++)
+        {
+            if (map[i][j] == 'S')
+            {
+                startX = i;
+                startY = j;
+            }
+            if (map[i][j] == 'E')
+            {
+                endX = i;
+                endY = j;
+            }
+        }
+    }
+
+    int score = dijkstra(map, startX, startY, endX, endY);
+    if (score != -1)
+    {
+        printf("Shortest path cost: %d\n", score);
+    }
+    else
+    {
+        printf("No path found.\n");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    ifstream input_file(argv[1]);
+    vector<vector<char>> map;
+
+    if (!input_file)
+    {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(input_file, line))
+    {
+        vector<char> row(line.begin(), line.end());
+        map.push_back(row);
+    }
+    input_file.close();
+
+    reindeer(map);
+}
+
--- day16/part1.cpp
+++ day16/part1.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <set>
 #include <queue>
+#include <climits>
 
 #define ULL unsigned long long
 
@@ -14,55 +15,42 @@ pair<int, int> dir(int direction)
 {
     switch (direction % 4)
     {
-        case 0:
-        {
-            return {0, 1};
-        }
-        case 1:
-        {
-            return {1, 0};
-        }
-        case 2:
-        {
-            return {0, -1};
-        }
-        case 3:
-        {
-            return {-1, 0};
-        }
+        case 0: return {0, 1};
+        case 1: return {1, 0};
+        case 2: return {0, -1};
+        case 3: return {-1, 0};
     }
 }
 
-
 ULL distance(const int& x, const int& y, const int& a, const int& b)
 {
-    return abs(x-a) + abs (y-b);
+    return abs(x - a) + abs(y - b);
 }
 
-struct State {
+struct State
+{
     int f, g, x, y, direction;
-    bool operator>(const State &other) const {
-        return f > other.f; 
+    bool operator>(const State &other) const
+    {
+        return f > other.f;
     }
 };
 
 ULL astar(const vector<vector<char>>& map, const int& x, const int& y, const int& dr, const int& a, const int& b)
 {
-    tuple<int, int, int> state = make_tuple(x, y, dr);
-     
+    int n = map.size();
+
+    vector<vector<vector<ULL>>> dist(n, vector<vector<ULL>>(n, vector<ULL>(4, ULLONG_MAX)));
+
     auto valid = [&map](const int& x, const int& y)
     {
         return (x >= 0 && x < map.size() && y >= 0 && y < map.size() && map[x][y] != '#');
     };
 
     priority_queue<State, vector<State>, greater<State>> openList;
-    set<tuple<int, int, int>> visited;
 
-
-    ULL h = distance(x, y, a, b);
-
-    openList.push({h, 0, x, y, dr});
-    visited.insert({x, y, dr});
+    dist[x][y][dr] = 0;
+    openList.push({0, 0, x, y, dr});
 
     while (!openList.empty())
     {
@@ -81,29 +69,24 @@ ULL astar(const vector<vector<char>>& map, const int& x, const int& y, const int
         int dx = pairDir.first, dy = pairDir.second;
         int nx = x + dx, ny = y + dy;
 
-        if (valid(nx, ny) && visited.find({nx, ny, dr}) == visited.end())
+        if (valid(nx, ny) && dist[nx][ny][dr] > g + 1)
         {
-            visited.insert({nx, ny, dr});
-            int newG = g+1;
-            int newF = newG + distance(nx, ny, a, b);
-            openList.push({newF, newG, nx, ny, dr});
+            dist[nx][ny][dr] = g + 1;
+            openList.push({g + 1, g + 1, nx, ny, dr});
         }
 
-        int newDr = (dr+1)%4;
-        if (visited.find({x, y, newDr}) == visited.end())
+        int newDr = (dr + 1) % 4;
+        if (dist[x][y][newDr] > g + 1000)
         {
-            visited.insert({x, y, newDr});
-            ULL newG = g+1000;
-            ULL newF = newG + distance(x, y, a, b);
-            openList.push({newF, newG, x, y, newDr});
+            dist[x][y][newDr] = g + 1000;
+            openList.push({g + 1000, g + 1000, x, y, newDr});
         }
-        newDr = (dr-1+4)%4;
-        if (visited.find({x, y, newDr}) == visited.end())
+
+        newDr = (dr - 1 + 4) % 4;
+        if (dist[x][y][newDr] > g + 1000)
         {
-            visited.insert({x, y, newDr});
-            ULL newG = g+1000;
-            ULL newF = newG + distance(x, y, a, b);
-            openList.push({newF, newG, x, y, newDr});
+            dist[x][y][newDr] = g + 1000;
+            openList.push({g + 1000, g + 1000, x, y, newDr});
         }
     }
     return -1;
@@ -118,11 +101,13 @@ void reindeer(vector<vector<char>>& map)
         {
             if (map[i][j] == 'S')
             {
-                x = i; y = j;
+                x = i; 
+                y = j;
             }
             if (map[i][j] == 'E')
             {
-                a = i; b = j;
+                a = i; 
+                b = j;
             }
         }
     }
@@ -131,12 +116,13 @@ void reindeer(vector<vector<char>>& map)
     int score = astar(map, x, y, dr, a, b);
     printf("score: %llu\n", score);
 }
-int main(int argc, char *argv[]) 
+
+int main(int argc, char *argv[])
 {
     ifstream input_file(argv[1]);
     vector<vector<char>> data;
 
-    if (!input_file) 
+    if (!input_file)
     {
         cerr << "error opening file" << endl;
         return 1;
@@ -144,7 +130,7 @@ int main(int argc, char *argv[])
 
     string line;
 
-    while (getline(input_file, line)) 
+    while (getline(input_file, line))
     {
         vector<char> row(line.begin(), line.end());
         data.push_back(row);
