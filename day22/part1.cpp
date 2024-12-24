dee377b39a45b7aec4877cae48f7393e2627e767
al-nf <alankjfung@gmail.com>
2024-12-23 22:16:29 -0500

    wtf is this?

---
+++ day22/part1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+#define ULL unsigned long long
+
+using namespace std;
+
+int calc(const int& n)
+{
+    int k = n;
+    k = ((k << 6) ^ k) & 0xFFFFFF;
+    k = ((k >> 5) ^ k) & 0xFFFFFF;
+    k = ((k << 11) ^ k) & 0xFFFFFF;
+    return k;
+}
+
+void solve(const vector<int>& nums)
+{
+    unordered_map<int, int> memo;
+    ULL sum = 0;
+
+    for (int num : nums)
+    {
+        int res = num;
+        for (int i = 0; i < 2000; i++)
+        {
+            if (memo.find(res) != memo.end())
+            {
+                res = memo[res];
+            }
+            else
+            {
+                res = calc(res);
+                memo[num] = res;
+            }
+        }
+        sum += res;
+    }
+
+    printf("sum: %llu\n", sum);
+}
+int main(int argc, char* argv[])
+{
+    ifstream inputFile(argv[1]);
+
+    vector<int> nums;
+
+    if (!inputFile)
+    {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(inputFile, line)) 
+    {
+        nums.push_back(stoi(line));
+    }
+
+    inputFile.close();
+
+    solve(nums);
+}
